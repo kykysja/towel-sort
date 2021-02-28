338f342f2bb70a923a22729f8ab3785dbfe258b4
kykysja <kykysja11@gmail.com>
2021-02-28 19:58:39 +0100

    feat: implement the function of the task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+module.exports = function towelSort(matrix) {
+	if (matrix === undefined) {
+		return [];
+	} else {
+		for (i = 1; i < matrix.length; i += 2) {
+			matrix[i] = matrix[i].reverse();
+		}
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+		matrix = matrix.flat(Infinity);
+		return matrix;
+	}
 }
